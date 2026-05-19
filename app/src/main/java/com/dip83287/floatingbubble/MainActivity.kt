51ada5b28fdbea7262353d31386b3c2e57d2a9e0
Dip Sarkar <bdrdip83287@gmail.com>
2026-05-19 19:01:50 +0600

    floting bubble with log Capture Success code

--- app/src/main/java/com/dip83287/floatingbubble/MainActivity.kt
+++ app/src/main/java/com/dip83287/floatingbubble/MainActivity.kt
@@ -3,6 +3,7 @@ package com.dip83287.floatingbubble
 import android.content.Intent
 import android.net.Uri
 import android.os.Build
+import android.os.Looper
 import android.os.Bundle
 import android.provider.Settings
 import android.widget.Toast
