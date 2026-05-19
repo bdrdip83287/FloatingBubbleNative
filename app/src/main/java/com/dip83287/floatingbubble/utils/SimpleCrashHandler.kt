fd5136661ed27edd2651bc0fcbdb49b9975f50b9
Dip Sarkar <bdrdip83287@gmail.com>
2026-05-19 22:03:09 +0600

    floting bubble with log Capture Success code

--- app/src/main/java/com/dip83287/floatingbubble/utils/SimpleCrashHandler.kt
+++ app/src/main/java/com/dip83287/floatingbubble/utils/SimpleCrashHandler.kt
@@ -5,26 +5,52 @@ import android.os.Handler
 import android.os.Looper
 import android.widget.Toast
 
-class SimpleCrashHandler(private val context: Context) : Thread.UncaughtExceptionHandler {
-    
+class SimpleCrashHandler(
+    private val context: Context
+) : Thread.UncaughtExceptionHandler {
+
     companion object {
-        private var defaultHandler: Thread.UncaughtExceptionHandler? = null
-        
+
+        private var defaultHandler:
+            Thread.UncaughtExceptionHandler? = null
+
         fun init(context: Context) {
-            defaultHandler = Thread.getDefaultUncaughtExceptionHandler()
-            Thread.setDefaultUncaughtExceptionHandler(SimpleCrashHandler(context.applicationContext))
+
+            defaultHandler =
+                Thread.getDefaultUncaughtExceptionHandler()
+
+            Thread.setDefaultUncaughtExceptionHandler(
+                SimpleCrashHandler(
+                    context.applicationContext
+                )
+            )
         }
     }
 
-    override fun uncaughtException(thread: Thread, throwable: Throwable) {
-        val log = SimpleLog.getInstance(context)
-        log.crash("CrashHandler", throwable)
-        
+    override fun uncaughtException(
+        thread: Thread,
+        throwable: Throwable
+    ) {
+
+        EmergencyLog.log(
+            "APP CRASH:\n\n" +
+            throwable.stackTraceToString()
+        )
+
         Handler(Looper.getMainLooper()).post {
-            Toast.makeText(context, "App crashed! Log saved.", Toast.LENGTH_LONG).show()
+
+            Toast.makeText(
+                context,
+                "App crashed! Log saved.",
+                Toast.LENGTH_LONG
+            ).show()
         }
-        
+
         Thread.sleep(500)
-        defaultHandler?.uncaughtException(thread, throwable)
+
+        defaultHandler?.uncaughtException(
+            thread,
+            throwable
+        )
     }
 }
