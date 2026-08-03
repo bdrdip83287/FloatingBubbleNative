93cdb8b85eb30e6b3fe2ab433935b71901212be5
Dip Sarkar <bdrdip83287@gmail.com>
2026-08-04 02:42:43 +0600

    Fix: unselect

--- app/src/main/java/com/dip83287/floatingbubble/FloatingBubbleService.kt
+++ app/src/main/java/com/dip83287/floatingbubble/FloatingBubbleService.kt
@@ -2001,7 +2001,7 @@ class FloatingBubbleService : Service() {
                             val dy = Math.abs(event.y - startY)
                             
                             // ✅ মুভমেন্ট ডিটেক্ট - 10px থ্রেশহোল্ড
-                            if (dx > 1 || dy > 1) {
+                            if (dx > 30 || dy > 30) {
                                 hasMoved = true
                                 isSingleTap = false
                                 cancelLongPress()
