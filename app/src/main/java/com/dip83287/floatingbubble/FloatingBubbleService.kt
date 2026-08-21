1a0bebf1cbd5bf25df37a16518f3b28643785408
Dip Sarkar <bdrdip83287@gmail.com>
2026-08-21 23:34:58 +0600

    Fix: unselec-7

--- app/src/main/java/com/dip83287/floatingbubble/FloatingBubbleService.kt
+++ app/src/main/java/com/dip83287/floatingbubble/FloatingBubbleService.kt
@@ -2472,7 +2472,7 @@ params.y =
                 setTextColor(Color.BLACK)
                 gravity = Gravity.CENTER
                 includeFontPadding = false
-                setPadding(0, 0, 0, 9)
+                setPadding(0, 0, 5, 0)
                 isClickable = true
                 isFocusable = true
                 layoutParams = LinearLayout.LayoutParams(dpToPx(buttonSize), dpToPx(buttonSize)).apply {
