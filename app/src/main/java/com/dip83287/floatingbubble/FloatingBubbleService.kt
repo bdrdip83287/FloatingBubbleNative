95119f338b66b7e7c649e4882c24eac431aa46cb
Dip Sarkar <bdrdip83287@gmail.com>
2026-08-30 06:51:44 +0600

    Final Ok

--- app/src/main/java/com/dip83287/floatingbubble/FloatingBubbleService.kt
+++ app/src/main/java/com/dip83287/floatingbubble/FloatingBubbleService.kt
@@ -4288,8 +4288,8 @@ setOnTouchListener(object : View.OnTouchListener {
             fun smallListButton(icon: Drawable): ImageButton =
     ImageButton(parent.context).apply {
         layoutParams = LinearLayout.LayoutParams(
-            dpToPx(22),
-            dpToPx(22)
+            dpToPx(24),
+            dpToPx(24)
         ).apply {
             gravity = Gravity.CENTER
         }
